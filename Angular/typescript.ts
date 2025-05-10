e676ec18987b550bbc37256de863eb76914322ed
Asma-malica <115386995+Asma-malica@users.noreply.github.com>
2025-05-10 13:25:02 +0530

    update

---
+++ Angular/typescript.ts
@@ -0,0 +1,68 @@
+// function log(message: string): void {
+//     console.log(message);
+//   }
+  
+//   log('success');
+  
+// function main(){
+//     for(var i=1;i<5;i++){
+//         console.log(i)
+//     }
+//     console.log('Finally with var : ',i)
+// }
+// main();
+
+
+// function main(){
+//     for(let i=1;i<5;i++){
+//         console.log(i)
+//     }
+//     console.log('Finally with let : ',i)
+// }
+// main();
+
+
+// let count ;
+// let a : number ;
+// let b : string ;
+// let c : boolean ;
+// let d : any ;
+// let e : number[] = [1,2,3] ;
+// let f : any[] = [1,'a',true]
+
+// enum Color{Red,Blue,Green}
+// let backgroundColor = Color.Red;
+
+// count = 'as'
+// count = 1
+// count = true
+
+
+// let color;
+// color = 'blue';
+// let end = color.endsWith('e')
+
+
+// let color;
+// color = 'blue';
+// let end = (<string>color).endsWith('e')
+// let alternate_end = (color as string).endsWith('e')
+
+//arrow function
+// let log = function(message){
+//     console.log(message)
+// }
+// let doLog = (message)=>{
+//     console.log(message)
+// }
+
+//interface
+// interface point{
+//     x:number,y:number,z:number
+// }
+// let draw = (point)=>{
+
+// }
+// draw({x:1,y:1,z:2})
+
+class
